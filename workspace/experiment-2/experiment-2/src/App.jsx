e853122b351e6c0826f067bc38c71080590ae7d2
Prince Goyal <princegoyalpg2005@gmail.com>
2026-01-14 10:08:01 +0530

    dist

--- workspace/experiment-2/experiment-2/src/App.jsx
+++ workspace/experiment-2/experiment-2/src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SinglePageApp from './components/Spa'
+import ButtonBasic from "./components/Button.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <SinglePageApp /> 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ButtonBasic />
     </>
   )
 }
