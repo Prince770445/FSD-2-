bc6e0977452cdcec58965eddda5e3e50c9780cb9
Armaanjot Kaur <armaanjot@gmail.com>
2026-03-25 22:38:41 +0530

    Added Experiment 11 microservices

--- micro-services-lab/customer-service/vir-customer/Scripts/activate.bat
+++ micro-services-lab/customer-service/vir-customer/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Downloads\exp-11\micro-services-lab\customer-service\vir-customer
+set VIRTUAL_ENV=D:\Downloads\FSD-2-\exp-11\micro-services-lab\customer-service\vir-customer
 
 if not defined PROMPT set PROMPT=$P$G
 
