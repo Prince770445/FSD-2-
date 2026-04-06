715bf5688c9687a34d1f8d50105ee8b6c3f67cbd
Prince Goyal <princegoyalpg2005@gmail.com>
2026-04-06 11:37:57 +0530

    exp13

--- Experiment-13/app.py
+++ Experiment-13/app.py
@@ -15,9 +15,9 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
-PORT = os.getenv("PORT", 3306)
+DB_PORT = os.getenv("DB_PORT")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{PORT}/{DB_NAME}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 db = SQLAlchemy(app)
 
